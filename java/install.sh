9c40ab00b413271ff23a79a6c09df467a3a47f4b
Niranjan Godbole <niranjan8192@gmail.com>
2018-03-09 10:32:53 +0000

    adds languages

---
+++ java/install.sh
@@ -0,0 +1,17 @@
+#!/bin/bash -e
+
+echo "================= Installing default-jdk & jre ==================="
+apt-get install -qq default-jre=2:1.8*
+apt-get install -qq default-jdk=2:1.8*
+
+echo "================= Installing openjdk-8-jdk ==================="
+add-apt-repository -y ppa:openjdk-r/ppa
+apt-get update -qq
+apt-get install -qq -y openjdk-8-jdk
+update-alternatives --set java /usr/lib/jvm/java-8-openjdk-armhf/jre/bin/java
+update-alternatives --set javac /usr/lib/jvm/java-8-openjdk-armhf/bin/javac
+
+add-apt-repository -y ppa:maarten-fonville/ppa
+apt-get update -qq
+apt-get install -qq icedtea-8-plugin
+update-alternatives --set javaws /usr/lib/jvm/java-8-openjdk-armhf/jre/bin/javaws
