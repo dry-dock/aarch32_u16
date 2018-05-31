25a6b4fb940cee225ec0c825adbf07ff4b8d312f
Devashish <devashish.86@gmail.com>
2018-05-31 11:13:50 +0530

    upgrading packages

--- java/install.sh
+++ java/install.sh
@@ -7,11 +7,10 @@ apt-get install -qq default-jdk=2:1.8*
 echo "================= Installing openjdk-8-jdk ==================="
 add-apt-repository -y ppa:openjdk-r/ppa
 apt-get update -qq
-apt-get install -qq -y openjdk-8-jdk
-update-alternatives --set java /usr/lib/jvm/java-8-openjdk-armhf/jre/bin/java
-update-alternatives --set javac /usr/lib/jvm/java-8-openjdk-armhf/bin/javac
-
+apt-get install -qq -y openjdk-8-jdk=8u162*
+update-alternatives --set java /usr/lib/jvm/java-8-openjdk-arm64/jre/bin/java
+update-alternatives --set javac /usr/lib/jvm/java-8-openjdk-arm64/bin/javac
 add-apt-repository -y ppa:maarten-fonville/ppa
 apt-get update -qq
-apt-get install -qq icedtea-8-plugin
-update-alternatives --set javaws /usr/lib/jvm/java-8-openjdk-armhf/jre/bin/javaws
+apt-get install -qq icedtea-8-plugin=1.6.2*
+update-alternatives --set javaws /usr/lib/jvm/java-8-openjdk-arm64/jre/bin/javaws
