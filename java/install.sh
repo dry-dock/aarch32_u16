9acedafa4234f5c699589569bdc9bbf792892713
Devashish <devashish.86@gmail.com>
2018-05-31 13:30:00 +0530

    fixing java

--- java/install.sh
+++ java/install.sh
@@ -8,9 +8,9 @@ echo "================= Installing openjdk-8-jdk ==================="
 add-apt-repository -y ppa:openjdk-r/ppa
 apt-get update -qq
 apt-get install -qq -y openjdk-8-jdk
-update-alternatives --set java /usr/lib/jvm/java-8-openjdk-arm64/jre/bin/java
-update-alternatives --set javac /usr/lib/jvm/java-8-openjdk-arm64/bin/javac
+update-alternatives --set java /usr/lib/jvm/java-8-openjdk-armhf/jre/bin/java
+update-alternatives --set javac /usr/lib/jvm/java-8-openjdk-armhf/bin/javac
 add-apt-repository -y ppa:maarten-fonville/ppa
 apt-get update -qq
 apt-get install -qq icedtea-8-plugin=1.6.2*
-update-alternatives --set javaws /usr/lib/jvm/java-8-openjdk-arm64/jre/bin/javaws
+update-alternatives --set javaws /usr/lib/jvm/java-8-openjdk-armhf/jre/bin/javaws
